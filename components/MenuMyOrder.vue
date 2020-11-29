cb905b4663dc7251950c4bc1c3e3632e4d50a8e7
Konstantinos Kotsos <kon.kotsos@gmail.com>
2020-11-29 18:10:39 +0000

    Add mixins on MenuMyOrder comp

--- components/MenuMyOrder.vue
+++ components/MenuMyOrder.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>MenuMyOrder</div>
+  <div>
+    <v-btn
+      text
+      class="btn btn--alt"
+      :class="{ 'btn--hidden-text': isMobileView }"
+      height="50px"
+      :ripple="false"
+      tag="a"
+      nuxt
+      link
+      x-small
+      :title="menuItem.label"
+      :to="menuItem.path"
+    >
+      <span v-if="!isMobileView" class="menu-item">{{ menuItem.label }}</span>
+      <v-icon size="13">fas fa-shopping-cart</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
+import mixins from '~/mixins'
+
 export default {
   name: 'MenuMyOrder',
+  mixins: [mixins],
+  props: {
+    menuItem: {
+      type: Object,
+      default() {
+        return {
+          name: 'my order',
+          label: 'My Order',
+          path: '#myorder',
+        }
+      },
+    },
+  },
   data() {
     return {}
   },
 }
 </script>
 
-<style></style>
+<style lang="scss" scopped>
+.menu-item {
+  margin-right: 10px;
+}
+</style>
