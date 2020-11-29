2c719ecacbe12b984ce458c4a59c175f44d1c3a6
Konstantinos Kotsos <kon.kotsos@gmail.com>
2020-11-29 18:09:32 +0000

    Add getHeaderMyOrderMenuItem on AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <v-app-bar fixed app height="110" color="transparent" elevation="0">
+    <v-app-bar
+      app
+      hide-on-scroll
+      height="110"
+      color="transparent"
+      elevation="0"
+    >
       <v-container>
         <v-row class="no-gutter d-flex justify-space-between align-center">
           <header-socials-list
@@ -19,7 +25,7 @@
             :menu-list="menuListRight"
           ></header-menu-list>
           <v-spacer></v-spacer>
-          <menu-my-order></menu-my-order>
+          <menu-my-order :menu-item="menuItemMyOrder"></menu-my-order>
         </v-row>
       </v-container>
 
@@ -27,8 +33,6 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-divider dark color="white" class="header-bottom-border"></v-divider>
 
     <v-navigation-drawer v-model="showSidebar" right temporary fixed>
       sidebar
@@ -43,28 +47,29 @@ import {
   getSocialsList,
   getHeaderMenuListLeft,
   getHeaderMenuListRight,
+  getHeaderMyOrderMenuItem,
 } from '~~/api/common.js'
+
+import mixins from '~/mixins'
 
 export default {
   name: 'AppHeader',
-
+  mixins: [mixins],
   data() {
     return {
       showSidebar: false,
       socialsList: [],
       menuListLeft: [],
       menuListRight: [],
+      menuItemMyOrder: {},
     }
   },
-  computed: {
-    isMobileView() {
-      return this.$vuetify.breakpoint.mobile
-    },
-  },
+
   async created() {
     this.socialsList = await getSocialsList()
     this.menuListLeft = await getHeaderMenuListLeft()
     this.menuListRight = await getHeaderMenuListRight()
+    this.menuItemMyOrder = await getHeaderMyOrderMenuItem()
   },
 }
 </script>
@@ -73,11 +78,13 @@ export default {
 .container {
   max-width: $container-width;
 }
-.header-bottom-border {
-  max-width: $container-width !important;
-  margin: auto;
+
+.container::after {
+  content: '';
+  display: block;
+  border-bottom: 1px solid #fff;
   position: relative;
-  top: 110px;
+  top: 29px;
   opacity: 0.3;
 }
 </style>
