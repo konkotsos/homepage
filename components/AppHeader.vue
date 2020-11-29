f489c79c87ede291ee94bab0591d615c143d76e9
Konstantinos Kotsos <kon.kotsos@gmail.com>
2020-11-29 16:29:48 +0000

    Add header bottom border

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <v-app-bar fixed app>
+    <v-app-bar fixed app height="110" color="transparent" elevation="0">
       <v-container>
-        <v-row class="d-flex justify-space-between">
+        <v-row class="no-gutter d-flex justify-space-between align-center">
           <header-socials-list
             :socials-list="socialsList"
           ></header-socials-list>
+          <v-spacer></v-spacer>
           <header-menu-list
             v-if="!isMobileView"
             :menu-list="menuListLeft"
           ></header-menu-list>
+          <v-spacer></v-spacer>
           <app-logo></app-logo>
+          <v-spacer></v-spacer>
           <header-menu-list
             v-if="!isMobileView"
             :menu-list="menuListRight"
           ></header-menu-list>
+          <v-spacer></v-spacer>
           <menu-my-order></menu-my-order>
         </v-row>
       </v-container>
+
       <v-btn v-if="isMobileView" icon @click.stop="showSidebar = !showSidebar">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
+
+    <v-divider dark color="white" class="header-bottom-border"></v-divider>
+
     <v-navigation-drawer v-model="showSidebar" right temporary fixed>
       sidebar
       <header-menu-list></header-menu-list>
@@ -50,7 +58,7 @@ export default {
   },
   computed: {
     isMobileView() {
-      return this.$vuetify.breakpoint.xsOnly
+      return this.$vuetify.breakpoint.mobile
     },
   },
   async created() {
@@ -64,5 +72,12 @@ export default {
 <style lang="scss">
 .container {
   max-width: $container-width;
+}
+.header-bottom-border {
+  max-width: $container-width !important;
+  margin: auto;
+  position: relative;
+  top: 110px;
+  opacity: 0.3;
 }
 </style>
