415ea6666ae7eca5917c9e154c8db6cf487b86d5
Konstantinos Kotsos <kon.kotsos@gmail.com>
2020-11-29 15:42:21 +0000

    App loho image and AppLogo comp

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>AppLogo</div>
+  <v-img max-height="33" max-width="84" :src="'/mhk-logo.png'"></v-img>
 </template>
 
 <script>
 export default {
   name: 'AppLogo',
   data() {
-    return {}
+    return {
+      logoPath: '~/assets/img/mhk-logo.png',
+    }
   },
 }
 </script>
