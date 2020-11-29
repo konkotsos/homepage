bce6a501f9433de6aaaf55f6083a58b321cc5b15
Konstantinos Kotsos <kon.kotsos@gmail.com>
2020-11-29 18:04:31 +0000

    Fix AppBody position

--- components/AppBody.vue
+++ components/AppBody.vue
@@ -1,8 +1,6 @@
 <template>
-  <v-main>
-    <v-container>
-      <nuxt />
-    </v-container>
+  <v-main class="app-body">
+    <nuxt />
   </v-main>
 </template>
 
@@ -15,4 +13,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.app-body {
+  position: relative;
+  top: -$app-header-height;
+}
+</style>
